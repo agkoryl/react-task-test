bfd0dd3f46e8e49895c49b5857d093af6b4896a9
Agnieszka Koryl <ag.koryl@gmail.com>
2019-04-14 13:40:22 +0200

    user list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+
 import './App.css';
+import Users from './components/Users/Users';
 
 class App extends Component {
+
+
+
   render() {
     return (
-      <div className="App">
-       
+      <div className="main-container">
+        <Users></Users>
       </div>
     );
   }
---
+++ src/components/Users/Users.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+
+import './Users.css';
+
+class Users extends React.Component {
+
+    state = {
+        users: []
+    }
+
+    fetchUsers() {
+        const users = [];
+        fetch('https://randomuser.me/api/?results=10')
+            .then(response => response.json())
+            .then(data => {
+                data.results.forEach(user => {
+                    users.push({ userId: user.login.uuid, userName: user.name.first, userLastName: user.name.last, userEmail: user.email, img: user.picture.large })
+                });
+                this.setState({ users });
+            })
+            .catch(error => { console.error('error'); });
+    }
+
+    componentDidMount() {
+        this.fetchUsers();
+    }
+
+    render() {
+        const {users} = this.state;
+
+        return (
+            <div className="content-container">
+                {users.map(user => 
+                            <div className="user-content-container" key={user.userId}>
+                                <div><img src={user.img} className="user-photo" alt="user-face"></img></div>
+                                <div className="user-details">
+                                <p>Imię: {user.userName}</p>
+                                <p>Nazwisko: {user.userLastName}</p>
+                                <p>Email: {user.userEmail}</p>
+                                </div>
+                            </div>
+                        )}
+        </div>
+        )
+    }
+
+
+
+}
+
+
+
+export default Users;
+
